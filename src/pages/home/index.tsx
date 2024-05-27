b0e9b7b7dfb977b3501f1fe128fb294cd3842d3f
João Henrique Portela <jhportla_rio32@hotmail.com>
2024-05-27 11:31:25 -0300

    CHORE[feature/home]: header and footer finalized

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
           </OrderByContainer>
         </SearchContainer>
         <MainContent>
-          Conteudo
+          Teste
         </MainContent>
 
         <Footer />
