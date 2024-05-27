0f3ced7aa59acf71983834c4248daf964154506c
João Henrique Portela <jhportla_rio32@hotmail.com>
2024-05-27 11:24:12 -0300

    CHORE[main]: implemented the project skeleton

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# CarRent-client
-Front-end criado com angular no estilo SPA consumindo uma api rest.
+# Next.js Boilerplate
+Description
+
+## Running the project
+
+1. Be sure you have **node** and **yarn** (or **npm**, if you use it) installed.
+2. Clone the repository by running
+```bash
+git clone github.com/joaohenriquebrs/CarRent-client.git
+```
+3. Install all the dependencies by running
+```bash
+yarn install
+# or
+npm install
+```
+4. To run the development server, run
+```bash
+yarn run dev
+# or  
+npm dev
+```
+5. Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Development team
+
+## Design team
+
---
+++ src/assets/index.ts
@@ -0,0 +1,131 @@
+import User from './user.png';
+import Lupa from './lupa.png';
+import CardCart from './cardCart.png';
+import RightArrow from './rightArrow.png';
+import WppLogo from './wppLogo.png';
+import WppLogo2 from './wppLogo2.jpg';
+import LogoHeader from './logoHeader.png';
+import AudiLogo from './audi.png';
+import BmwLogo from './bmw.png';
+import CherryLogo from './chery.png';
+import ChrevroletLogo from './chevrolet.png';
+import FiatLogo from './fiat.png';
+import FordLogo from './ford.png';
+import HondaLogo from './honda.png';
+import JeepLogo from './jeep.png';
+import MercedesLogo from './mercedes.png';
+import NissanLogo from './nissan.png';
+import PeugeotLogo from './peugeot.png';
+import RenaultLogo from './renault.png';
+import ToyotaLogo from './toyota.png';
+import VolkswagenLogo from './volkswagen.png';
+import BannerFirst from './bannerFirst.jpg';
+import OrderIcon from './orderIcon.png';
+import FaceIcon from './faceIcon.png';
+import InstaIcon from './instaIcon.png';
+import LocaltionIcon from './localIcon.jpg';
+import TeleIcon from './teleIcon.jpg';
+import EmailIcon from './emailIcon.png';
+import WppFooterIcon from './wppIconFooter.jpg';
+import AutoConf from './autoconf.png';
+import FiestaImage from './fiestaImage.png';
+import TcrossImage from './tcrossImage.png';
+import RenagadeImage from './renegadeImage.png';
+import YearIcon from './ano.png';
+import KmIcon from './km.png';
+import IconShift from './iconShift.png';
+import IconCalendar from './iconCalendar.png';
+import IconColor from './iconColor.png';
+import IconKm from './iconKm.png';
+import IconDiesel from './iconDiesel.png';
+import IconOk from './iconOk.png';
+import IconWpp from './iconWpp.png';
+import IconPhone from './iconPhone.png';
+import IconLocation from './iconLocation.png';
+import RenegadeMainImage from './renegadeMainImage.png';
+import RenegadeSecondImage from './renegadeSecondImage.png';
+import RenegadeThirdImage from './renegadeThirdImage.png';
+import RenegadeFourthImage from './renegadeFourthImage.png';
+import RenegadeFifthImage from './renegadeFifthImage.png';
+import RenegadeSexthImage from './renegadeSexthImage.png';
+import ServiceImage from './serviceImage.png';
+import LogoWppService from './logoWppService.png';
+import StepsImage1 from './stepsImage1.png';
+import StepsImage2 from './stepsImage2.png';
+import StepsImage3 from './stepsImage3.png';
+import BradescoLogo from './bradescoLogo.png';
+import ItauLogo from './itauLogo.png';
+import SantanderLogo from './santanderLogo.png';
+import PanLogo from './panLogo.png';
+import SafraLogo from './safraLogo.png';
+import BvLogo from './bvLogo.png';
+import IconLupaFinance from './iconLupaFinance.png';
+import IconPencilDocument from './iconPencilDocument.png';
+import IconProfileDocument from './iconProfileDocument.png';
+
+export {
+  User,
+  Lupa,
+  CardCart,
+  RightArrow,
+  LogoHeader,
+  WppLogo,
+  WppLogo2,
+  AudiLogo,
+  BmwLogo,
+  CherryLogo,
+  ChrevroletLogo,
+  FiatLogo,
+  FordLogo,
+  HondaLogo,
+  JeepLogo,
+  MercedesLogo,
+  NissanLogo,
+  PeugeotLogo,
+  RenaultLogo,
+  ToyotaLogo,
+  VolkswagenLogo,
+  BannerFirst,
+  OrderIcon,
+  FaceIcon,
+  InstaIcon,
+  LocaltionIcon,
+  TeleIcon,
+  EmailIcon,
+  WppFooterIcon,
+  AutoConf,
+  FiestaImage,
+  TcrossImage,
+  RenagadeImage,
+  YearIcon,
+  KmIcon,
+  IconShift,
+  IconCalendar,
+  IconColor,
+  IconKm,
+  IconDiesel,
+  IconOk,
+  IconWpp,
+  IconPhone,
+  IconLocation,
+  RenegadeMainImage,
+  RenegadeSecondImage,
+  RenegadeThirdImage,
+  RenegadeFourthImage,
+  RenegadeFifthImage,
+  RenegadeSexthImage,
+  ServiceImage,
+  LogoWppService,
+  StepsImage1,
+  StepsImage2,
+  StepsImage3,
+  BradescoLogo,
+  ItauLogo,
+  SantanderLogo,
+  PanLogo,
+  SafraLogo,
+  BvLogo,
+  IconLupaFinance,
+  IconPencilDocument,
+  IconProfileDocument
+};
