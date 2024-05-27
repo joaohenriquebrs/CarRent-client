297b5f13a267a96385ddd86c2a4db9c6da148adb
João Henrique Portela <jhportla_rio32@hotmail.com>
2024-05-27 11:51:45 -0300

    CHORE[feature/sellYourCar]: finalized sellYourCar and about pages

---
+++ src/pages/sellYourCar/styles.ts
@@ -0,0 +1,222 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  height: 100vh;
+  font-family: Montserrat, sans-serif;
+`;
+
+export const MainContent = styled.div`
+  margin-top: 123px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TitlesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #fafafa;
+  height: 170px;
+  width: 100%;
+  margin-bottom: 80px;
+`;
+
+export const TitleMain = styled.div`
+  font-size: 32px;
+  color: #616161;
+  margin-bottom: 10px;
+  font-weight: 600;
+`;
+
+export const SubtitleMain = styled.div`
+  font-size: 18px;
+  color: #424242;
+  font-weight: 600;
+`;
+
+export const FormsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 76%;
+  margin-bottom: 60px;
+`;
+
+export const FormsContent = styled.div`
+  width: 75%;
+`;
+
+export const CarDatas = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const TitleCarDatas = styled.div`
+  font-size: 15px;
+  color: #757575;
+  font-weight: 700;
+  margin-bottom: 15px;
+`;
+
+export const InputCarDatas = styled.input`
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  width: 65%;
+  margin-bottom: 5px;
+  padding: 11px 0px 11px 10px;
+`;
+
+export const ExampleCarDatas = styled.div`
+  font-size: 12px;
+  color: #616161;
+`;
+
+export const TitleContactDatas = styled.div`
+  font-size: 15px;
+  color: #757575;
+  font-weight: 700;
+  margin-bottom: 15px;
+`;
+
+export const ContactDatas = styled.div`
+  color: #616161;
+  font-size: 14px;
+  margin-bottom: 12px;
+`;
+
+export const ContactNameContainer = styled.div``;
+
+export const TitleContactName = styled.div`
+  margin-bottom: 8px;
+`;
+
+export const InputContactName = styled.input`
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  width: 65%;
+  margin-bottom: 5px;
+  padding: 11px 0px 11px 10px;
+`;
+
+export const ContactPhoneContainer = styled.div``;
+
+export const TitleContactPhone = styled.div`
+  margin-bottom: 8px;
+`;
+
+export const InputContactPhone = styled.input`
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  width: 65%;
+  margin-bottom: 5px;
+  padding: 11px 0px 11px 10px;
+`;
+
+export const ContactEmailContainer = styled.div``;
+
+export const TitleContactEmail = styled.div`
+  margin-bottom: 8px;
+`;
+
+export const InputContactEmail = styled.input`
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  width: 65%;
+  margin-bottom: 5px;
+  padding: 11px 0px 11px 10px;
+`;
+
+export const ButtonSend = styled.div`
+  background: #6a6c6a;
+  width: 65%;
+  display: flex;
+  justify-content: center;
+  padding: 14px 0px;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 0.3px;
+`;
+
+export const NextStepsContainer = styled.div`
+  width: 25%;
+`;
+
+export const TitleNextSteps = styled.div`
+  font-size: 16px;
+  color: #757575;
+  font-weight: 600;
+  margin-bottom: 45px;
+`;
+
+export const BlockNextSteps = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: #616161;
+  font-size: 15px;
+  margin-bottom: 45px;
+  letter-spacing: 0.3px;
+`;
+
+export const BlockImage = styled.div`
+  margin-right: 20px;
+`;
+
+export const OptionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #f5f5f5;
+`;
+
+export const TitleOptions = styled.div`
+  color: #616161;
+  font-size: 28px;
+  font-weight: 600;
+  margin: 65px 0px 20px 0px;
+`;
+
+export const BlockOptions = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 96%;
+  gap: 25px;
+  height: 400px;
+`;
+
+export const Options = styled.div`
+  width: 33.3%;
+  height: 76%;
+  margin-bottom: 35px;
+  background: #fff;
+  padding: 15px 10px 0px 15px;
+  border-radius: 8px;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+`;
+
+export const CardNumber = styled.div`
+  color: #6a6c6a;
+  font-size: 40px;
+  font-weight: 600;
+  margin-bottom: 15px;
+`;
+
+export const Title = styled.div`
+  color: #424242;
+  font-size: 20px;
+  margin-bottom: 15px;
+  font-weight: 600;
+  letter-spacing: 0.6px;
+`;
+
+export const Description = styled.div`
+  color: #616161;
+  font-size: 19px;
+  letter-spacing: 0.6px;
+  line-height: 28px;
+`;
