992faeeeee960a99d72e9abcbfc2e07e00c73fcf
João Henrique Portela <jhportla_rio32@hotmail.com>
2024-05-28 01:27:14 -0300

    CHORE[feature/finalCarPageAdjusts]: adjusted carouselPhotos responsiveness

--- src/components/carouselPhotos/styles.ts
+++ src/components/carouselPhotos/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export const CarouselContainer = styled.div`
+  margin-bottom: 40px;
+  display: flex;
   align-items: center;
   justify-content: center;
-  position: relative;
-  margin-bottom: 190px;
+  flex-direction: column;
 
   @media (min-width: 700px) and (max-width: 800px) {
     margin-bottom: 120px;
@@ -33,36 +34,7 @@ export const CardCarousel = styled.div`
   scroll-behavior: smooth;
   overflow: hidden;
   width: 90vw;
-  
-  @media (min-width: 950px) and (max-width: 1200px) {
-    width: 80vw;
-    margin-left: 40px;
-  }
-
-  @media (min-width: 800px) and (max-width: 950px) {
-    width: 75vw;
-    margin-left: 60px;
-  }
-
-  @media (min-width: 650px) and (max-width: 800px) {
-    width: 70vw;
-    margin-left: 70px;
-  }
-
-  @media (min-width: 500px) and (max-width: 650px) {
-    width: 65vw;
-    margin-left: 80px;
-  }
-
-  @media (min-width: 400px) and (max-width: 500px) {
-    width: 60vw;
-    margin-left: 75px;
-  }
-
-  @media (min-width: 300px) and (max-width: 400px) {
-    width: 60vw;
-    margin-left: 60px;
-  }
+  margin-bottom: 20px;
 `;
 
 export const BlockImage = styled.a`
@@ -117,12 +89,9 @@ export const ButtonRight = styled.button`
   background-color: transparent;
   border: none;
   cursor: pointer;
-  position: absolute;
-  right: -65px;
-  min-width: 30px;
-  min-height: 30px;
-  max-width: 40px;
-  max-height: 40px;
+  width: 50px;
+  height: 50px;
+  transition: transform 0.3s ease;
 `;
 
 export const ButtonLeft = styled.button`
@@ -130,71 +99,14 @@ export const ButtonLeft = styled.button`
   background-color: transparent;
   border: none;
   cursor: pointer;
-  position: left;
-  position: absolute;
-  left: -65px;
-  min-width: 30px;
-  min-height: 30px;
-  max-width: 40px;
-  max-height: 40px;
+  width: 50px;
+  height: 50px;
 `;
 
 export const ButtonContainer = styled.div`
-  position: relative;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   width: 100%;
-  width: 90vw;
-  margin-top: -190px;
-
-  @media (min-width: 1200px) and (max-width: 1500px) {
-    margin-top: -130px;
-  }
-
-  @media (min-width: 950px) and (max-width: 1200px) {
-    width: 80vw;
-    margin-left: 40px;
-    margin-top: -120px;
-  }
-
-  @media (min-width: 768px) and (max-width: 950px) {
-    width: 78vw;
-    margin-left: 45px;
-    margin-top: -95px;
-  }
-
-  @media (min-width: 700px) and (max-width: 768px) {
-    width: 78vw;
-    margin-left: 45px;
-    margin-top: -100px;
-  }
-
-  @media (min-width: 600px) and (max-width: 700px) {
-    width: 78vw;
-    margin-left: 45px;
-    margin-top: -130px;
-    margin-bottom: 150px;
-  }
-
-  @media (min-width: 500px) and (max-width: 600px) {
-    width: 78vw;
-    margin-left: 45px;
-    margin-top: -105px;
-    margin-bottom: 150px;
-  }
-
-  @media (min-width: 400px) and (max-width: 500px) {
-    width: 60vw;
-    margin-left: 73px;
-    margin-top: -90px;
-    margin-bottom: 120px;
-  }
-
-  @media (min-width: 300px) and (max-width: 400px) {
-    width: 50vw;
-    margin-left: 78px;
-    margin-top: -90px;
-    margin-bottom: 110px;
-  }
+  gap: 50px;
 `;
